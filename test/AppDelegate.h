3067adf21761c2619e4df22e46928dfe4e3db3d6
zl <zl@appledeMacBook-Pro-2.local>
2019-04-28 07:53:47 +0800

    注释

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by zl on 2019/4/28.
+//  Copyright © 2019 zl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
